73436281f10b223843a1731cb1043f45487ce8d2
js <jan.stanczyk.ext@nokia.com>
2016-11-10 11:52:26 +0100

    Changed implementation.h Jan Stanczyk

--- 20-type-template/implementation.h
+++ 20-type-template/implementation.h
@@ -23,3 +23,28 @@ T _value;
 };
 
 
+
+
+
+template<typename T>
+class container_wrapper<boost::optional<T>>
+{
+public:
+container_wrapper() = default;
+
+container_wrapper(boost::optional<T> arg): _value(std::move(arg))
+{
+}
+container_wrapper(const container_wrapper &)=default;
+container_wrapper(container_wrapper &&) = default;
+container_wrapper & operator=(const container_wrapper &) = default;
+container_wrapper & operator=(container_wrapper &&)=default;
+
+std::size_t size() const
+{
+return _value ? 1:0;
+}
+private:
+boost::optional<int> _value;
+};
+
